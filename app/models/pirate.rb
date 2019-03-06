432c25104b707f8aea791534947b50d0cc5aca8e
William J Teskey <willjteskey@gmail.com>
2019-03-06 21:40:11 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
